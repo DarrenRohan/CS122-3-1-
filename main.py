8bf97d46e5f43a879dc13002ad849012aa14755f
Darren <darrenrohan64@gmail.com>
2024-09-25 16:07:09 -0700

    Finished first branch of code

---
+++ main.py
@@ -0,0 +1,19 @@
+# gets number inputs
+num1 = input("Enter number here: ")  
+num2 = input("Enter number here: ") 
+num3 = input("Enter number here: ") 
+
+#checks number input
+if num1 > num2:
+    if num1 > num3:
+        gnum = num1
+        if num2 > num3:
+            mnum = num2
+            snum = num3
+            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number")
+        else:
+            mnum = num3
+            snum = num2
+            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+    else:
+        gnum = num3, mnum = num1, snum = num2
