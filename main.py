e6c97c30268ecc920f6243413f4666d16955bb44
Darren <darrenrohan64@gmail.com>
2024-09-25 16:10:37 -0700

    Tentatively finished, will test

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ if num1 > num2:
             print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
     else:
         gnum = num3, mnum = num1, snum = num2
+        print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+else:
+    if num2 > num3:
+        gnum = num2
+        if num1 > num3:
+            mnum = num1
+            snum = num3
+            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+        else:
+            mnum = num3
+            snum = num1
+            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+    else:
+        gnum = num3, mnum = num2, snum = num1
+        print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
