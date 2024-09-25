28b5eb8497b0e0665053db422bc53ddd46f7f2cd
Darren <darrenrohan64@gmail.com>
2024-09-25 16:14:41 -0700

    Bugfixing issues with line 19

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # gets number inputs
-num1 = input("Enter number here: ")  
-num2 = input("Enter number here: ") 
-num3 = input("Enter number here: ") 
+num1 = int(input("Enter number here: "))
+num2 = int(input("Enter number here: "))
+num3 = int(input("Enter number here: "))
 
 #checks number input
 if num1 > num2:
@@ -10,25 +10,25 @@ if num1 > num2:
         if num2 > num3:
             mnum = num2
             snum = num3
-            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number")
+            print (gnum, " is the largest number.", mnum, " is the second largest.", snum, " is the smallest number.")
         else:
             mnum = num3
             snum = num2
-            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+            print (gnum, " is the largest number.", mnum, " is the second largest.", snum, " is the smallest number.") 
     else:
         gnum = num3, mnum = num1, snum = num2
-        print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+        print (gnum, " is the largest number.", mnum, " is the second largest.", snum, " is the smallest number.") 
 else:
     if num2 > num3:
         gnum = num2
         if num1 > num3:
             mnum = num1
             snum = num3
-            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+            print (gnum, " is the largest number.", mnum, " is the second largest.", snum, " is the smallest number.") 
         else:
             mnum = num3
             snum = num1
-            print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+            print (gnum, " is the largest number.", mnum, " is the second largest.", snum, " is the smallest number.") 
     else:
         gnum = num3, mnum = num2, snum = num1
-        print (gnum, " is the largest number.", mnum, " is the second largest", snum, " is the smallest number") 
+        print (gnum, " is the largest number.", mnum, " is the second largest.", snum, " is the smallest number.") 
